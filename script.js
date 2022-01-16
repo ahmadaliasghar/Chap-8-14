910130b0b8f327fd3a8934851ccc24ebcabf68a9
ahmadaliasghar <88629200+ahmadaliasghar@users.noreply.github.com>
2022-01-16 15:49:24 +0500

    Assignment 2 Done

--- script.js
+++ script.js
@@ -26,12 +26,100 @@ document.getElementById("askNameFromUser").onclick = function() {
 }
 
 // Chapter 11 Comparison Operators 
-document.getElementById("comparisonOperators").onclick = function() {
+document.getElementById("comparisonOperators").onclick = function() 
+{
 
-    var num1 = 16;
-    var num2 = 15;
+    var num1 = 15;
+    var num2 = 16;
 
     if (num1 > num2) {
         alert("Yes, Your Condition is True")
+    }else{ 
+        alert("No, Your Condition is not True") 
     }
+
+    // // Greater than or Equal 
+    // var num3 = 15;
+    // var num4 = 15;
+
+    // if (num3 >= num4) {
+    //     alert("Yes, Your condition is true. The 15 = 15")
+    // }
+}
+
+document.getElementById("ifElseIf").onclick = function() {
+
+    var num1 = 10;
+    var num2 = 15
+    if (num1 >= num2) {
+        alert("Yes, your condition id true")
+    } else if (num1 == num2) {
+        alert("Yes, your second condition is true.")
+    } else if (num1 <= num2) {
+        alert("Yes, your third condition is true.")
+    }
+    else {
+        alert("No, your condition is false.")
+    }
+}
+
+document.getElementById("testingSetsOfConditions").onclick = function() {
+
+    var age = prompt("Your Age?");
+    var weight = prompt("Your Weight?");
+
+    if(age >= 18 && weight <= 70 ) {
+
+        alert("You are a smart man.")
+
+    } else if (age >= 18 && weight > 70) {
+
+        alert("You are a fat man.")
+    }
+    else  {
+        alert("You are a baby.")
+    }
+}
+
+
+document.getElementById("ifStatementsNested").onclick = function() {
+
+
+    var age = prompt("Your age?");
+
+    if (age >= 18) {
+
+        var weight = prompt("Your weight?");
+
+        if (weight <= 70) {
+            alert("You are a smart man.")
+        } else {
+            alert("You are a fat man.")
+        }
+    } else {
+        alert("You are a baby.");
+    }
+}
+
+document.getElementById("login").onclick = function() {
+
+    var userName = prompt("Your Username");
+    var password = prompt("Your Password"); 
+
+    if (userName === "ahmad" && password === "123"){
+        alert("You ar logged in")
+    }
+    else {
+        alert("Username or Password Incorrect!")
+    }
+}
+
+
+// Clear Statement 
+function clrStatement() {
+    document.getElementById("statement").innerHTML = " ";
+}
+// Clear Output / Result 
+function clrOutput() {
+    document.getElementById("result").innerHTML = " ";
 }
